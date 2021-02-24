5776f6d3668c69fecb11f742b0966c05a677b8b6
张志豪 <izhangzhihao@hotmail.com>
2021-02-24 18:13:04 +0800

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token: string = core.getInput('github_token')
+    const userName: string = core.getInput('delete_user_name')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(token)
 
-    core.setOutput('time', new Date().toTimeString())
+    const issues = await octokit.paginate(octokit.issues.list, {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo
+    })
+
+    for (const issue of issues) {
+      const resp = await octokit.issues.listComments({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: issue.number
+      })
+
+      const comments = resp.data.filter(it => it.user?.login === userName)
+
+      for (const comment of comments) {
+        console.log(
+          `Processing issue ${comment.issue_url} user: ${comment.user} comment: ${comment.body_text}`
+        )
+
+        await octokit.request(
+          'DELETE /repos/{owner}/{repo}/issues/comments/{comment_id}',
+          {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            comment_id: comment.id
+          }
+        )
+      }
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
