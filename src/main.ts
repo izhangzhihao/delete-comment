d2109593804a744f8ac80193721dbdf8a9c2ca5e
Bharat Kunwar <brtknr@bath.edu>
2023-06-30 07:19:43 +0530

    Add body_regex option for deleting comments (#38)

--- src/main.ts
+++ src/main.ts
@@ -8,39 +8,24 @@ async function run(): Promise<void> {
   try {
     const token: string = core.getInput('github_token')
     const userName: string = core.getInput('delete_user_name')
+    const bodyRegex: string = core.getInput('body_regex')
     const issueNumber: number = parseInt(core.getInput('issue_number'))
-
     const octokit = github.getOctokit(token)
 
-    const issues: number[] = []
-
-    if (issueNumber) {
-      issues.push(issueNumber)
-    } else {
-      console.log('issue_number not provided, will clean all the issues.')
-      const allIssues = await octokit.paginate(
-        'GET /repos/:owner/:repo/issues?state=all',
-        {
-          owner: github.context.repo.owner,
-          repo: github.context.repo.repo
-        }
-      )
-
-      issues.push(...allIssues.map((value: any) => value['number']))
-    }
-
-    for (const issue of issues) {
+    const deleteComments = async (issue: number): Promise<void> => {
       const resp = await octokit.issues.listComments({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         issue_number: issue
       })
 
-      const comments = resp.data.filter(it => it.user?.login === userName)
+      const comments = resp.data.filter(
+        it => it.user?.login === userName && it.body?.match(bodyRegex)
+      )
 
       for (const comment of comments) {
         console.log(
-          `Processing issue ${comment.issue_url} user: ${comment.user} comment: ${comment.body_text}`
+          `Processing issue ${comment.issue_url} user: ${comment.user?.login} comment: ${comment.body}`
         )
 
         await octokit.request(
@@ -53,6 +38,8 @@ async function run(): Promise<void> {
         )
       }
     }
+
+    await deleteComments(issueNumber)
   } catch (error) {
     console.error(error)
     console.error(error.stack)
